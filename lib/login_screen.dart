e3754b9c63c20eadf0c50804efab4b9bd4940fe2
Arwa-Hatem <arwahatem93@gmail.com>
2023-08-12 15:23:42 +0300

    task3

---
+++ lib/login_screen.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+class login_screen extends StatefulWidget {
+  const login_screen({super.key});
+  @override
+  State<login_screen> createState() => _login_screenState();
+}
+
+class _login_screenState extends State<login_screen> {
+  bool checkboxvalue = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.green,
+      child: Column(children: [
+        SizedBox(height: 50 // MediaQuery.of(context).size.height * 0.1,
+            ),
+        Container(
+          padding: EdgeInsets.only(top: 3, bottom: 10),
+          child: Image.asset(
+            "images/cartoon.png",
+            height: MediaQuery.of(context).size.height * 0.2,
+          ),
+        ),
+        Spacer(),
+        Container(
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 237, 237, 232),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50), topRight: Radius.circular(50)),
+          ),
+          height: MediaQuery.of(context).size.height * 0.6,
+          width: MediaQuery.of(context).size.width * 1,
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: [
+              const Text(
+                "Login",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: "Username",
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  prefixIcon: Icon(Icons.person),
+                  contentPadding: EdgeInsets.symmetric(vertical: 15),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: "Password",
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.visibility_off),
+                  contentPadding: EdgeInsets.symmetric(vertical: 15),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Text(
+                    'New to Quizz app?',
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        '  Register',
+                        style: TextStyle(color: Colors.green),
+                      )),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Login'),
+                style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(10),
+                    elevation: 15,
+                    shadowColor: Color.fromARGB(255, 86, 86, 85),
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    fixedSize: Size(150, 10)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Icon(
+                Icons.fingerprint,
+                size: 50,
+                color: Colors.blue,
+              ),
+              const Text(
+                "Use Touch ID",
+                style: TextStyle(color: Color.fromARGB(255, 142, 141, 141)),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: <Widget>[
+                  Checkbox(
+                      value: checkboxvalue,
+                      activeColor: Colors.green,
+                      onChanged: (bool? newValue) {
+                        setState(() {
+                          checkboxvalue = newValue!;
+                        });
+                      }),
+                  const Text("Remember me"),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Forget password?',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
+    ));
+  }
+}
--- lib/opening_screen.dart
+++ lib/opening_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//import 'package:google_fonts/google_fonts.dart';
 
 class OpeningScreen extends StatelessWidget {
   const OpeningScreen({super.key});
@@ -8,95 +7,64 @@ class OpeningScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: MediaQuery.of(context).size.width, // the width of the device
-        height: MediaQuery.of(context).size.height * 1, // hight of the device
-
+        width: double.infinity,
+        height: double.infinity,
         decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage("images/background.jpg"), fit: BoxFit.cover)),
-
-        // ignore: prefer_const_constructors, prefer_const_literals_to_create_immutables
         child: Column(
-          // mainAxisSize: MainAxisSize.min,
           children: [
-            /// first widget
-            ///
-
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.3,
             ),
-
-            // if (MediaQuery.of(context).orientation == Orientation.portrait)
-            Image.asset(
-              "Images/quizz.png",
-              height: MediaQuery.of(context).size.height * 0.2,
+            Container(
+              padding: EdgeInsets.only(left: 110, bottom: 10),
+              child: Image.asset(
+                "images/quizz.png",
+                height: MediaQuery.of(context).size.height * 0.4,
+              ),
             ),
-
-            // if (MediaQuery.of(context).orientation == Orientation.landscape)
-            //   Image.asset(
-            //     "Images/quizLogo.png",
-            //     height: MediaQuery.of(context).size.height * 0.3,
-            //   ),
-
-            /// second widget :
-
-            const Text("ITI Quiz App",
-                style: TextStyle(
-                  color: Colors.yellow,
-                  fontFamily: 'Pacifico',
-                )),
-
+            const Text(
+              "ITI Quiz app",
+              style: TextStyle(
+                color: Colors.yellow,
+                height: -4,
+                fontSize: 30,
+                fontFamily: 'Pacifico',
+              ),
+            ),
             const SizedBox(
-              height: 20,
+              height: 90,
             ),
-
             const Text(
               "We are creative, enjoy our app",
               style: TextStyle(
                 color: Colors.white,
                 height: -6,
-                fontSize: 28,
+                fontSize: 29,
                 fontFamily: 'DancingScript',
               ),
             ),
-
-            // SizedBox(
-            //   height: 100,
-            // ),
-            Spacer(),
-
-            // SizedBox(
-            //   height: double.infinity,
-            // ),
-
+            const Spacer(),
             Container(
-                margin: EdgeInsets.symmetric(horizontal: 12, vertical: 20),
-                width: double.infinity,
-                child: ElevatedButton(onPressed: () {}, child: Text("Start"))),
-
-            // Container(
-            //   margin: EdgeInsets.all(20), // المسافة بين الشيء ومحيطه الخارجي
-            //   padding: EdgeInsets.all(40), // المسافة بين الشيء ومحيطه الداخلي
-            //   height: 300,
-            //   width: double.infinity,
-            //   decoration: BoxDecoration(
-            //       color: Colors.red,
-            //       border: Border.all(width: 2, color: Colors.white)),
-            //   child: Text(
-            //       "dqnqwkjnjklnqwdqnqwkjnjklnqwdqnqwkjnjklnqwdqnqwkjnjklnqwdqnqwkjnjklnqwdqnqwkjnjklnqwdqnqwkjnjklnqwdqnqwkjnjklnqwdqnqwkjnjklnqwdqnqwkjnjklnqwdqnqwkjnjklnqw"),
-            // )
+              alignment: Alignment.bottomCenter,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Start",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  padding: EdgeInsets.symmetric(horizontal: 150),
+                ),
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-/////////
-///               Column, Row, Sizedbox, Contair
