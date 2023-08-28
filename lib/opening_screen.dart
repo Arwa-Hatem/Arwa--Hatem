f362e5efe15325ed3aa7849b5ddd025922e621ae
Arwa-Hatem <arwahatem93@gmail.com>
2023-08-28 17:39:47 +0300

    animation

--- lib/opening_screen.dart
+++ lib/opening_screen.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:task2/login_screen.dart';
 
-class OpeningScreen extends StatelessWidget {
+class OpeningScreen extends StatefulWidget {
   const OpeningScreen({super.key});
+
+  @override
+  State<OpeningScreen> createState() => _OpeningScreenState();
+}
+
+class _OpeningScreenState extends State<OpeningScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _slidelogocontroller;
+  late AnimationController _fadetextcontroller;
+  @override
+  void initState() {
+    super.initState();
+    _slidelogocontroller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _fadetextcontroller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    _slidelogocontroller.forward();
+    _fadetextcontroller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,30 +39,41 @@ class OpeningScreen extends StatelessWidget {
             ),
             Container(
               padding: const EdgeInsets.only(left: 110, bottom: 10),
-              child: Image.asset(
-                "images/quizz.png",
-                height: MediaQuery.of(context).size.height * 0.4,
+              child: SlideTransition(
+                position: Tween<Offset>(begin: Offset(0, -1), end: Offset(0, 0))
+                    .animate(_slidelogocontroller),
+                child: Image.asset(
+                  "images/quizz.png",
+                  height: MediaQuery.of(context).size.height * 0.4,
+                ),
               ),
             ),
-            const Text(
-              "ITI Quiz app",
-              style: TextStyle(
-                color: Colors.yellow,
-                height: -4,
-                fontSize: 30,
-                fontFamily: 'Pacifico',
-              ),
-            ),
-            const SizedBox(
-              height: 90,
-            ),
-            const Text(
-              "We are creative, enjoy our app",
-              style: TextStyle(
-                color: Colors.white,
-                height: -6,
-                fontSize: 29,
-                fontFamily: 'DancingScript',
+            FadeTransition(
+              opacity: _fadetextcontroller,
+              child: Column(
+                children: [
+                  const Text(
+                    "ITI Quiz app",
+                    style: TextStyle(
+                      color: Colors.yellow,
+                      height: -4,
+                      fontSize: 30,
+                      fontFamily: 'Pacifico',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 90,
+                  ),
+                  const Text(
+                    "We are creative, enjoy our app",
+                    style: TextStyle(
+                      color: Colors.white,
+                      height: -6,
+                      fontSize: 29,
+                      fontFamily: 'DancingScript',
+                    ),
+                  ),
+                ],
               ),
             ),
             const Spacer(),
--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -13,122 +13,137 @@ class quiz_screen extends StatefulWidget {
   State<quiz_screen> createState() => _quiz_screenState();
 }
 
-class _quiz_screenState extends State<quiz_screen> {
+class _quiz_screenState extends State<quiz_screen>
+    with SingleTickerProviderStateMixin {
   int index = 0;
   int score = 0;
+  late AnimationController _fadetextcontroller;
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    _fadetextcontroller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+    _fadetextcontroller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: const Color.fromARGB(255, 89, 144, 104),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("${index + 1}/${(widget.categorymap["data"] as List).length}"),
-            Text(widget.categorymap["CategoryName"]),
-            Icon(Icons.quiz)
-          ],
+    return FadeTransition(
+      opacity: _fadetextcontroller,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: const Color.fromARGB(255, 89, 144, 104),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                  "${index + 1}/${(widget.categorymap["data"] as List).length}"),
+              Text(widget.categorymap["CategoryName"]),
+              Icon(Icons.quiz)
+            ],
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 50,
-            ),
-            Center(
-              child: Container(
-                alignment: const Alignment(-1, -1),
-                padding: const EdgeInsets.all(20),
-                margin: const EdgeInsets.only(right: 20, left: 20),
-                decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 223, 239, 226),
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Color.fromARGB(255, 176, 191, 176),
-                          spreadRadius: 1,
-                          blurRadius: 5,
-                          offset: Offset(5, 5)),
-                    ]),
-                child: Column(
-                  children: [
-                    Text(
-                      "Question ${index + 1} :",
-                      style: TextStyle(
-                          fontFamily: 'Pacifico',
-                          fontSize: 20,
-                          decoration: TextDecoration.underline),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      widget.categorymap["data"][index]["question"],
-                      style: TextStyle(fontFamily: 'Pacifico', fontSize: 20),
-                    )
-                  ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: Container(
+                  alignment: const Alignment(-1, -1),
+                  padding: const EdgeInsets.all(20),
+                  margin: const EdgeInsets.only(right: 20, left: 20),
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 223, 239, 226),
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromARGB(255, 176, 191, 176),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                            offset: Offset(5, 5)),
+                      ]),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Question ${index + 1} :",
+                        style: TextStyle(
+                            fontFamily: 'Pacifico',
+                            fontSize: 20,
+                            decoration: TextDecoration.underline),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        widget.categorymap["data"][index]["question"],
+                        style: TextStyle(fontFamily: 'Pacifico', fontSize: 20),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "choices is :",
-              style: TextStyle(fontFamily: 'Pacifico', fontSize: 25),
-            ),
-            for (int i = 0;
-                i < (widget.categorymap["data"][index]["answers"]).length;
-                i++)
-              GestureDetector(
-                  onTap: () {
-                    score += widget.categorymap["data"][index]["answers"][i]
-                        ["score"] as int;
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "choices is :",
+                style: TextStyle(fontFamily: 'Pacifico', fontSize: 25),
+              ),
+              for (int i = 0;
+                  i < (widget.categorymap["data"][index]["answers"]).length;
+                  i++)
+                GestureDetector(
+                    onTap: () {
+                      score += widget.categorymap["data"][index]["answers"][i]
+                          ["score"] as int;
 
-                    if (index + 1 <
-                        (widget.categorymap["data"] as List).length) {
-                      setState(() {
-                        index++;
-                      });
-                    } else {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute<void>(
-                          builder: (BuildContext context) => score_screen(
-                            finalscore: score,
-                            numque: index + 1,
+                      if (index + 1 <
+                          (widget.categorymap["data"] as List).length) {
+                        setState(() {
+                          index++;
+                        });
+                      } else {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) => score_screen(
+                              finalscore: score,
+                              numque: index + 1,
+                            ),
                           ),
+                        );
+                      }
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      height: MediaQuery.of(context).size.height * 0.1,
+                      decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 223, 239, 226),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromARGB(255, 176, 191, 176),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: Offset(5, 5)),
+                          ]),
+                      child: Center(
+                          child: Text(
+                        widget.categorymap["data"][index]["answers"][i]["ans"],
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontFamily: 'Pacifico',
                         ),
-                      );
-                    }
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.all(10),
-                    height: MediaQuery.of(context).size.height * 0.1,
-                    decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 223, 239, 226),
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Color.fromARGB(255, 176, 191, 176),
-                              spreadRadius: 1,
-                              blurRadius: 5,
-                              offset: Offset(5, 5)),
-                        ]),
-                    child: Center(
-                        child: Text(
-                      widget.categorymap["data"][index]["answers"][i]["ans"],
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 25,
-                        fontFamily: 'Pacifico',
-                      ),
+                      )),
                     )),
-                  )),
-          ],
+            ],
+          ),
         ),
       ),
     );
